894072ae5920d3eb6cd795f4e999413fb16f99e0
mysteriou <elio07@hotmail.fr>
2023-02-13 09:33:07 +0100

    ajout route post login et sinup dans server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+
+app.post('/api/auth/signup', (req, res, next) => {
+ 
+  console.log("singup");
+
+});
+
+
+app.post('/api/auth/login', (req, res, next) => {
+ 
+  console.log("singup");
+
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
