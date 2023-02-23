9ef0164ff3d683af3cdeb918abc89e8434e5660b
mysteriou <elio07@hotmail.fr>
2023-02-23 11:06:02 +0100

    decoupage en fonction l'add sauces

--- add_sauces.js
+++ add_sauces.js
@@ -1,12 +1,87 @@
 module.exports = {
 
-    add_sauces: function(req,res,data){
+    add_file: function(){
+
+        const multer  = require('multer');
+
+        var storage = multer.diskStorage({
+
+            destination:function(request, file, callback)
+            {
+                callback(null, './upload');
+            },
+            filename : function(request, file, callback)
+            {
+                var temp_file_arr = file.originalname.split(".");
+    
+                var temp_file_name = temp_file_arr[0];
+    
+                var temp_file_extension = temp_file_arr[1];
+    
+                callback(null, temp_file_name + '-' + Date.now() + '.' + temp_file_extension+".png");
+            }
+    
+        });
+
+        return storage;
+
+    },
+
+    add_sauces: function(req,res){
 
         let MongoClient = require('mongodb').MongoClient;
         let connectionUrl = "mongodb://localhost:27017/p6_oc";
         const jwt = require('jsonwebtoken');
 
-        let da = data;
+        var pic = req.files;
+
+        var namepic = pic[0].originalname;
+      
+        var namepic = namepic.split(".");
+      
+        var extpic = namepic[1];
+      
+        var namefile = pic[0].filename;
+      
+      
+        var full_name = namefile+extpic;
+      
+          var tab = req.body.sauce;
+      
+          var tab1 = tab.split('{');
+      
+          var tab2 = tab1[1];
+      
+          var tab2a = tab1[1].split("}");
+      
+          var b = tab2a[0];
+      
+          
+          var b1 = b.split(",");
+          
+          
+           a = [];
+      
+           a2 = [];
+      
+           for(a = 0; a <= b1.length-1; a++){
+      
+            var a1 = b1[a].split(":");
+             
+      
+            a2.push(a1[1]);
+      
+           }
+           
+    
+           const token = req.headers.authorization.split(' ')[1];
+           const decodedToken = jwt.verify(token, 'shhhhh');
+           const userId = decodedToken.userId;
+      
+        var data = { userId: a2[5], name : a2[0],  manufacturer : a2[1], description : a2[2], mainPepper : a2[3], 
+            "imageUrl" :namefile+"."+extpic, heat : a2[4], like : 0,  dislikes : 0, usersLiked:0, usersDisliked: 0,} 
+      
+      
 
           
         /*connection a la base  donn*/
@@ -18,7 +93,7 @@ module.exports = {
             
             
 
-                    db.collection("sauces").insertOne(da, function(err, res) {
+                    db.collection("sauces").insertOne(data, function(err, res) {
                        
                         client.close();
                             
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const { application } = require('express');
 
 const add_sauce = require("./add_sauces.js");
 
-const multer  = require('multer')
+const multer  = require('multer');
+const { add_file } = require('./add_sauces.js');
 const upload = multer({ dest: 'uploads/' })
 
 
@@ -28,78 +29,12 @@ app.use((req, res, next) => {
 
 app.post('/api/sauces', upload.any(), function (req, res, next) {
 
- var storage = multer.diskStorage({
-
-		destination:function(request, file, callback)
-		{
-			callback(null, './upload');
-		},
-		filename : function(request, file, callback)
-		{
-			var temp_file_arr = file.originalname.split(".");
-
-			var temp_file_name = temp_file_arr[0];
-
-			var temp_file_extension = temp_file_arr[1];
-
-			callback(null, temp_file_name + '-' + Date.now() + '.' + temp_file_extension+".png");
-		}
-
-	});
+ 
+  add_sauce.add_file();
 
   
-  var pic = req.files;
-
-  var namepic = pic[0].originalname;
-
-  var namepic = namepic.split(".");
-
-  var extpic = namepic[1];
-
-  var namefile = pic[0].filename;
-
-
-  var full_name = namefile+extpic;
-
-    var tab = req.body.sauce;
-
-    var tab1 = tab.split('{');
-
-    var tab2 = tab1[1];
-
-    var tab2a = tab1[1].split("}");
-
-    var b = tab2a[0];
-
-    
-    var b1 = b.split(",");
-    
-    
-     a = [];
-
-     a2 = [];
-
-     for(a = 0; a <= b1.length-1; a++){
-
-      var a1 = b1[a].split(":");
-       
-
-      a2.push(a1[1]);
-
-     }
-     
-
-
-     const jwt = require('jsonwebtoken');
-     const token = req.headers.authorization.split(' ')[1];
-     const decodedToken = jwt.verify(token, 'shhhhh');
-     const userId = decodedToken.userId;
-
-     let data = { userId: a2[5], name : a2[0],  manufacturer : a2[1], description : a2[2], mainPepper : a2[3], "imageUrl" :namefile+"."+extpic, 
-     heat : a2[4], like : 0,  dislikes : 0, usersLiked:0, usersDisliked: 0,} 
-
-
-      add_sauce.add_sauces(req,res,data);
+  
+      add_sauce.add_sauces(req,res);
 
     // Everything went fine.
   
@@ -142,6 +77,7 @@ app.get('/api/sauces', (req, res, next) => {
   res.end();
 
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
