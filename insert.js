13b4079dc7396005a766463c55e429b43cd8e285
mysteriou <elio07@hotmail.fr>
2023-04-10 12:32:24 +0200

    ajoute mongose error a la fonction singup

--- insert.js
+++ insert.js
@@ -1,6 +1,7 @@
 function signup(req, hash, res) {
   const uniqueValidator = require('mongoose-unique-validator');
   const mongoose = require('mongoose');
+  const MongooseError = require('mongoose/lib/error');
 
   // Connexion à la base de données p6_oc
   mongoose.connect('mongodb://localhost/p6_oc', {
@@ -33,31 +34,38 @@ function signup(req, hash, res) {
     User.findOne({ email: req.body.email }, (err, existingUser) => {
       if (err) {
         console.log('Error while checking email uniqueness: ' + err.message);
-        res.end();
+        res.status(500).json({ message: 'Une erreur est survenue lors de la vérification de l\'email.' });
       } else if (existingUser) {
         console.log('Email already exists');
-        res.end();
+        res.status(400).json({ message: 'Cet email est déjà utilisé. Veuillez en choisir un autre.' });
       } else {
-        // Si l'email est unique, créer une nouvelle instance de l'utilisateur
-        const new_user = new User({
+        // Si l'e-mail est unique, créer un nouveau document User avec les données de la requête
+        const newUser = new User({
           email: req.body.email,
           password: hash
         });
         
-        // Enregistrer le nouvel utilisateur dans la base de données
-        new_user.save((err) => {
+        // Enregistrer le document dans la collection User
+        newUser.save((err, savedUser) => {
           if (err) {
-            console.log('Error while saving new user: ' + err.message);
+            // Si une erreur survient lors de l'enregistrement, vérifier s'il s'agit d'une erreur de validation unique
+            if (err instanceof MongooseError.ValidationError && err.errors.email) {
+              console.log('Email uniqueness validation failed: ' + err.errors.email.message);
+              res.status(400).json({ message: err.errors.email.message });
+            } else {
+              console.log('Error while saving user: ' + err.message);
+              res.status(500).json({ message: 'Une erreur est survenue lors de la création de votre compte.' });
+            }
           } else {
-            console.log('New user created');
+            console.log('User saved successfully');
+            res.status(201).json({ message: 'Votre compte a été créé avec succès.' });
           }
-          res.end();
         });
       }
     });
-  }).catch((error) => {
-    console.log('Error connecting to database: ' + error.message);
-    res.end();
+  }).catch((err) => {
+    console.log('Error while connecting to database: ' + err.message);
+    res.status(200).json({err});
   });
 }
 
