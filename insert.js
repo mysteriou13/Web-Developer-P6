ff0d7673222887fdced9a4f6673bc58d2a126527
mysteriou <elio07@hotmail.fr>
2023-04-10 15:18:56 +0200

    ajouter mongose error la fonction login

--- insert.js
+++ insert.js
@@ -1,7 +1,23 @@
+
+ const mongoose = require('mongoose');
+ const MongooseError = require('mongoose/lib/error');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+  // Ajouter d'autres propriétés à votre schéma User si nécessaire
+});
+
 function signup(req, hash, res) {
   const uniqueValidator = require('mongoose-unique-validator');
-  const mongoose = require('mongoose');
-  const MongooseError = require('mongoose/lib/error');
+ 
 
   // Connexion à la base de données p6_oc
   mongoose.connect('mongodb://localhost/p6_oc', {
@@ -10,20 +26,7 @@ function signup(req, hash, res) {
   }).then(() => {
     console.log('Connected to database');
     
-    // Définir le schéma pour la collection User
-    const userSchema = new mongoose.Schema({
-      email: {
-        type: String,
-        unique: true,
-        required: true
-      },
-      password: {
-        type: String,
-        required: true
-      }
-      // Ajouter d'autres propriétés à votre schéma User si nécessaire
-    });
-    
+
     // Appliquer le plugin uniqueValidator au schéma User
     userSchema.plugin(uniqueValidator, { message: 'Erreur, email déjà existant.' });
     
@@ -71,80 +74,56 @@ function signup(req, hash, res) {
 
     
     /* function login*/
-
-     function login (email,pass,res) {
-
-        let MongoClient = require('mongodb').MongoClient;
-  let connectionUrl = "mongodb://localhost:27017/p6_oc";
-  const bcrypt = require('bcrypt');
-  var jwt = require('jsonwebtoken');
-
- let t = 0;
-
-  const saltRounds = 10;
-
-  
-  // creating the message object
-  let user = {"email" : email, "pass": pass,"resut":null, "stat":res.status(200)};
-
-          
-  /*connection a la base  donn*/
-  MongoClient.connect(connectionUrl, function(err, client,res) {
-      if (err) throw err;
-  
-  
-      // if database and collection do not exist they are created */
+    function login(email, pass, res) {
+      let MongoClient = require('mongodb').MongoClient;
+      let connectionUrl = "mongodb://localhost:27017/p6_oc";
+      const bcrypt = require('bcrypt');
+      var jwt = require('jsonwebtoken');
+      const { Error } = require('mongoose'); // import Error from mongoose
       
-      var db = client.db('p6_oc')
-
-  
-
-  db.collection("User").findOne({email:email }, function(err,obj,res) {
-
-    /*verif email existe*/
-    
-    if(!obj){
-
-      user.stat.status(401).json({ error: 'error mail non trouver' });
-
-    }
-
-      /*verif mot de pass user*/
-
-     
-      bcrypt.compare(pass,obj.password, function(err, result) {
-          if (result) {
+      let t = 0;
+      const saltRounds = 10;
       
-            var token = jwt.sign({ token: obj._id }, 'shhhhh');
-
-            userId = obj._id;
-        
-           user.stat.status(200).json(
-            
-            {
-                userId : obj._id,
-                token: token,
-            
-            }
-            
-            );
-            
-          }else{
-
-              user.stat.status(401).json({error: 'error mot de passe'});
+      // creating the message object
+      let user = { "email": email, "pass": pass, "resut": null, "stat": res.status(200) };
       
-          }
+      /*connection a la base  donn*/
+      MongoClient.connect(connectionUrl, function (err, client, res) {
+          if (err) throw err;
+      
+          // if database and collection do not exist they are created */
+          var db = client.db('p6_oc')
+      
+          db.collection("User").findOne({ email: email }, function (err, obj, res) {
+              /*verif email existe*/
+              if (!obj) {
+                  const mongooseErr = new Error('error mail non trouver');
+                  mongooseErr.name = 'MongooseError'; // set the error name
+                  user.stat.status(401).json({ error: mongooseErr.message });
+              }
+      
+              /*verif mot de pass user*/
+              bcrypt.compare(pass, obj.password, function (err, result) {
+                  if (result) {
+                      var token = jwt.sign({ token: obj._id }, 'shhhhh');
+                      userId = obj._id;
+                      user.stat.status(200).json(
+                          {
+                              userId: obj._id,
+                              token: token,
+                          }
+                      );
+                  } else {
+                      const mongooseErr = new Error('error mot de passe');
+                      console.log(mongooseErr.message);
+                      mongooseErr.name = 'MongooseError'; // set the error name
+                      user.stat.status(401).json({ error: mongooseErr.message });
+                  }
+              });
           });
-
-      
-      
-  });
-
-   
-
-  });
-        
-    }
+      });
+  }
+  
 
     module.exports = {
         signup: signup,
