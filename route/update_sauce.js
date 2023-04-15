58c0597264a01d9ce09de53f6c501348f6cc7d03
mysteriou <elio07@hotmail.fr>
2023-04-15 15:39:54 +0200

    verif si req file est défine dans update sauce

--- route/update_sauce.js
+++ route/update_sauce.js
@@ -19,69 +19,46 @@ const { verifyToken } = require('../verif_token.js');
 
 router.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-router.put('/api/sauces/:id',verifyToken ,upload.single('image'), function (req, res, next) {
+router.put('/api/sauces/:id', verifyToken, upload.single('image'), function (req, res, next) {
 
-  var ext = req.file.originalname;
-
-var ext1 = ext.split(".");
-
-
-
-
-
-  MongoClient.connect(url, function(err, client) {
-
-
+  MongoClient.connect(url, function (err, client) {
     const db = client.db('p6_oc');
-    db.collection(nomDeLaCollection).findOne({_id: ObjectId(req.params.id)}, function(err, result) {
+    db.collection(nomDeLaCollection).findOne({ _id: ObjectId(req.params.id) }, function (err, result) {
+      if (err) throw err;
 
-
-      fs.unlink('./uploads/'+result.namefile, (err) => {
-     
-        console.log('Le fichier a été supprimé avec succès');
-      });
-
-      client.close();
+      if (req.file) {
+        // Si un fichier a été téléchargé, supprime l'ancien fichier et renomme le nouveau
+        const oldPath = './uploads/' + result.namefile;
+        const newPath = './uploads/' + req.file.filename + path.extname(req.file.originalname);
+        fs.unlink(oldPath, (err) => {
+        
+          console.log('Le fichier a été supprimé avec succès');
+          fs.rename(req.file.path, newPath, (err) => {
+            if (err) throw err;
+            console.log('Le fichier a été renommé avec succès');
+            // Met à jour la sauce avec les nouvelles données
+            const thingObject = {
+              ...JSON.parse(req.body.sauce),
+              imageUrl: `${req.protocol}://${req.get('host')}/uploads/${req.file.filename + path.extname(req.file.originalname)}`
+            };
+            const sauce = require("../Sauce.js");
+            sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+              .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+              .catch(error => res.status(401).json({ error }));
+            client.close();
+          });
+        });
+      } else {
+        // Si aucun fichier n'a été téléchargé, met à jour la sauce avec les nouvelles données
+        const thingObject = { ...req.body };
+        const sauce = require("../Sauce.js");
+        sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet modifié!' }))
+          .catch(error => res.status(401).json({ error }));
+        client.close();
+      }
     });
   });
-
-
-
-  const thingObject = req.file ? {
-    ...JSON.parse(req.body.sauce),
-    imageUrl: `${req.protocol}://${req.get('host')}/uploads/${req.file.filename+"."+ext1[1]}`
-  } : { ...req.body };
-
-var ext = req.file.originalname;
-
-var name_ext = ext.split(".");
-
-
-console.log(name_ext);
-
-
-
-const oldPath = './uploads/'+req.file.filename;
-const newPath = './uploads/'+req.file.filename+"."+name_ext[1];
-
-fs.rename(oldPath, newPath, (err) => {
-  if (err) throw err;
-  console.log('Le fichier a été renommé avec succès');
 });
 
-  
-
-  var sauce = require("../Sauce.js");
-
-   sauce.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
-    .then(() => res.status(200).json({message : 'Objet modifié!'}))
-    .catch(error => res.status(401).json({ error }));
-
-  });
-
-
-
-
 module.exports = router;
-
-
