076ab5367eee339288fb8606ec5d4f358940fb83
mysteriou <elio07@hotmail.fr>
2023-04-16 17:06:01 +0200

    mise en mongose fonction singup

--- insert.js
+++ insert.js
@@ -1,72 +1,55 @@
-
-function signup(req,hash,res) {
-
-  let MongoClient = require('mongodb').MongoClient;
-  let connectionUrl = "mongodb://localhost:27017/p6_oc";
-  const uniqueValidator = require('mongoose-unique-validator');
+function signup(req, hash, res) {
   const mongoose = require('mongoose');
+
+  // Connexion à la base de données
+  mongoose.connect('mongodb://localhost:27017/p6_oc', { useNewUrlParser: true });
   
-  // creating the message object
-  let user = {"email" : req.body.email, "password": hash};
+  // Définition du schéma de la collection User
+ 
+  // Création du modèle User
   
-  // connect to MongoDB
-  MongoClient.connect(connectionUrl, function(err, client) {
-    if (err) throw err;
+  // Création d'un nouvel utilisateur à insérer dans la base de données
+  const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  });
+
+
+
+  const User = mongoose.model('User', userSchema);
   
-    // if database and collection do not exist they are created
-    var db = client.db('p6_oc');
+  // Insertion du nouvel utilisateur dans la collection User
+  const newUser = new User({
+
+    email: req.body.email,
+  password: hash
+  });
   
-    // define the user schema
-    const userSchema = new mongoose.Schema({
-      email: { type: String, required: true, unique: true },
-      password: { type: String, required: true }
-    });
-  
-    // add the unique validator plugin to the schema
-    userSchema.plugin(uniqueValidator, { message: 'Erreur, email déjà existant.' });
-  
-    // define the User model
-    const User = mongoose.models.User || mongoose.model('User', userSchema);
-  
-    // create a new user instance
-    const new_user = new User({
-      email: req.body.email,
-      password: hash
-    });
-  
-    // find the user with the specified email
-    db.collection("User").findOne({ email: req.body.email }, function(err, obj) {
-      if (err) throw err;
-  
-      // if the user does not exist, insert the new user into the database
-      if (obj === null) {
-        db.collection("User").insertOne(new_user, function(err, res) {
-          if (err) throw err;
-  
-          console.log("User inserted");
-          client.close();
-        });
-      } else {
-        console.log("User already exists");
-        client.close();
-      }
-    });
+  // Insertion du nouvel utilisateur dans la collection User
+  newUser.save((err, user) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`Utilisateur ${user.name} inséré dans la collection User`);
+    }
   });
   
   res.end();
-  
-    }
-    
+
+}
+
+
+
     /*login user*/
 
-    function login(email, pass, res) {
+    function login (email, pass, res) {
       const mongoose = require('mongoose');
       const bcrypt = require('bcrypt');
       var jwt = require('jsonwebtoken');
-    
+
       // creating the message object
       let user = { "email": email, "pass": pass, "resut": null, "stat": res.status(200) };
-    
+
       /*connection a la base  donn*/
       mongoose.connect('mongodb://localhost:27017/p6_oc', { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
         if (err) {
@@ -77,11 +60,11 @@ function signup(req,hash,res) {
           });
           return;
         }
-    
+
         // if database and collection do not exist they are created */
         var db = mongoose.connection;
-    
-        db.collection("User").findOne({ email: email }, function (err, obj) {
+
+        db.collection('users').findOne({ email: email }, function (err, obj) {
           if (err) {
             console.log('Database query error:', err);
             user.stat.status(500).json({
@@ -90,7 +73,7 @@ function signup(req,hash,res) {
             });
             return;
           }
-    
+
           /*verif email existe*/
           if (!obj) {
             user.stat.status(401).json({
@@ -98,7 +81,7 @@ function signup(req,hash,res) {
             });
             return;
           }
-    
+
           /*verif mot de pass user*/
           bcrypt.compare(pass, obj.password, function (err, result) {
             if (err) {
@@ -109,7 +92,7 @@ function signup(req,hash,res) {
               });
               return;
             }
-    
+
             if (result) {
               var token = jwt.sign({ token: obj._id }, 'shhhhh');
               userId = obj._id;
