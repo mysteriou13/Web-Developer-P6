e62f847b2fc45d7d8ef7d0be2304ea956dad0074
mysteriou <elio07@hotmail.fr>
2023-04-16 15:53:09 +0200

    mise en mongose login user

--- insert.js
+++ insert.js
@@ -1,125 +1,132 @@
 
- const mongoose = require('mongoose');
- const MongooseError = require('mongoose/lib/error');
+function signup(req,hash,res) {
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-  // Ajouter d'autres propriétés à votre schéma User si nécessaire
-});
-
-function signup(req, hash, res) {
+  let MongoClient = require('mongodb').MongoClient;
+  let connectionUrl = "mongodb://localhost:27017/p6_oc";
   const uniqueValidator = require('mongoose-unique-validator');
- 
-
-  // Connexion à la base de données p6_oc
-  mongoose.connect('mongodb://localhost/p6_oc', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(() => {
-    console.log('Connected to database');
-    
-
-    // Appliquer le plugin uniqueValidator au schéma User
+  const mongoose = require('mongoose');
+  
+  // creating the message object
+  let user = {"email" : req.body.email, "password": hash};
+  
+  // connect to MongoDB
+  MongoClient.connect(connectionUrl, function(err, client) {
+    if (err) throw err;
+  
+    // if database and collection do not exist they are created
+    var db = client.db('p6_oc');
+  
+    // define the user schema
+    const userSchema = new mongoose.Schema({
+      email: { type: String, required: true, unique: true },
+      password: { type: String, required: true }
+    });
+  
+    // add the unique validator plugin to the schema
     userSchema.plugin(uniqueValidator, { message: 'Erreur, email déjà existant.' });
-    
-    // Créer un modèle Mongoose pour la collection User à partir du schéma défini
-    const User = mongoose.model('User', userSchema);
-    
-    // Utiliser la méthode findOne de Mongoose pour vérifier la disponibilité de l'e-mail
-    User.findOne({ email: req.body.email }, (err, existingUser) => {
-      if (err) {
-        console.log('Error while checking email uniqueness: ' + err.message);
-        res.status(500).json({ message: 'Une erreur est survenue lors de la vérification de l\'email.' });
-      } else if (existingUser) {
-        console.log('Email already exists');
-        res.status(400).json({ message: 'Cet email est déjà utilisé. Veuillez en choisir un autre.' });
+  
+    // define the User model
+    const User = mongoose.models.User || mongoose.model('User', userSchema);
+  
+    // create a new user instance
+    const new_user = new User({
+      email: req.body.email,
+      password: hash
+    });
+  
+    // find the user with the specified email
+    db.collection("User").findOne({ email: req.body.email }, function(err, obj) {
+      if (err) throw err;
+  
+      // if the user does not exist, insert the new user into the database
+      if (obj === null) {
+        db.collection("User").insertOne(new_user, function(err, res) {
+          if (err) throw err;
+  
+          console.log("User inserted");
+          client.close();
+        });
       } else {
-        // Si l'e-mail est unique, créer un nouveau document User avec les données de la requête
-        const newUser = new User({
-          email: req.body.email,
-          password: hash
-        });
-        
-        // Enregistrer le document dans la collection User
-        newUser.save((err, savedUser) => {
-          if (err) {
-            // Si une erreur survient lors de l'enregistrement, vérifier s'il s'agit d'une erreur de validation unique
-            if (err instanceof MongooseError.ValidationError && err.errors.email) {
-              console.log('Email uniqueness validation failed: ' + err.errors.email.message);
-              res.status(400).json({ message: err.errors.email.message });
-            } else {
-              console.log('Error while saving user: ' + err.message);
-              res.status(500).json({ message: 'Une erreur est survenue lors de la création de votre compte.' });
-            }
-          } else {
-            console.log('User saved successfully');
-            res.status(201).json({ message: 'Votre compte a été créé avec succès.' });
-          }
-        });
+        console.log("User already exists");
+        client.close();
       }
     });
-  }).catch((err) => {
-    console.log('Error while connecting to database: ' + err.message);
-    res.status(200).json({err});
   });
-}
-
+  
+  res.end();
+  
+    }
     
-    /* function login*/
+    /*login user*/
+
     function login(email, pass, res) {
       const mongoose = require('mongoose');
       const bcrypt = require('bcrypt');
       var jwt = require('jsonwebtoken');
-  
+    
       // creating the message object
       let user = { "email": email, "pass": pass, "resut": null, "stat": res.status(200) };
-  
+    
       /*connection a la base  donn*/
       mongoose.connect('mongodb://localhost:27017/p6_oc', { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
-          if (err) throw err;
-  
-          // if database and collection do not exist they are created */
-          var db = mongoose.connection;
-  
-          db.collection("User").findOne({ email: email }, function (err, obj) {
-              /*verif email existe*/
-              if (!obj) {
-                  const mongooseErr = new Error('error mail non trouver');
-                  mongooseErr.name = 'MongooseError'; // set the error name
-                  user.stat.status(401).json({ error: mongooseErr.message });
-              }
-  
-              /*verif mot de pass user*/
-              bcrypt.compare(pass, obj.password, function (err, result) {
-                  if (result) {
-                      var token = jwt.sign({ token: obj._id }, 'shhhhh');
-                      userId = obj._id;
-                      user.stat.status(200).json(
-                          {
-                              userId: obj._id,
-                              token: token,
-                          }
-                      );
-                  } else {
-                      const mongooseErr = new Error('error mot de passe');
-                      console.log(mongooseErr.message);
-                      mongooseErr.name = 'MongooseError'; // set the error name
-                      user.stat.status(401).json({ error: mongooseErr.message });
-                  }
-              });
+        if (err) {
+          console.log('Database connection error:', err);
+          user.stat.status(500).json({
+            message: 'Database connection error',
+            error: err
           });
+          return;
+        }
+    
+        // if database and collection do not exist they are created */
+        var db = mongoose.connection;
+    
+        db.collection("User").findOne({ email: email }, function (err, obj) {
+          if (err) {
+            console.log('Database query error:', err);
+            user.stat.status(500).json({
+              message: 'Database query error',
+              error: err
+            });
+            return;
+          }
+    
+          /*verif email existe*/
+          if (!obj) {
+            user.stat.status(401).json({
+              message: 'Invalid email or password'
+            });
+            return;
+          }
+    
+          /*verif mot de pass user*/
+          bcrypt.compare(pass, obj.password, function (err, result) {
+            if (err) {
+              console.log('Password comparison error:', err);
+              user.stat.status(500).json({
+                message: 'Password comparison error',
+                error: err
+              });
+              return;
+            }
+    
+            if (result) {
+              var token = jwt.sign({ token: obj._id }, 'shhhhh');
+              userId = obj._id;
+              user.stat.status(200).json({
+                userId: obj._id,
+                token: token,
+              });
+            } else {
+              user.stat.status(401).json({
+                message: 'Invalid email or password'
+              });
+            }
+          });
+        });
       });
-  }
-  
-  
+    }
+    
 
     module.exports = {
         signup: signup,
--- login_data.js
+++ login_data.js
@@ -1,11 +1,38 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-
-// schéma of a sauce
-const sauceSchema = mongoose.Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model("User", sauceSchema);
+// hash the password before saving it to the database
+userSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
